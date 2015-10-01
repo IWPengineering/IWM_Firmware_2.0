60ca8006515e08321b021cdb4b2beb995c6d9163
KenKok <kk1311@messiah.edu>
2015-10-01 12:41:14 -0400

    Added queue.c and queue.h
    Fully tested queue functions

---
+++ mcc_generated_files/queue.c
@@ -0,0 +1,88 @@
+/*
+ * File:   queue.c
+ * Author: KSK0419
+ *
+ * Created on October 1, 2015, 11:41 AM
+ */
+
+
+#include "xc.h"
+#include "mcc.h"
+#include "queue.h"
+
+bool InitQueue(queue *queueP, uint8_t queueSize)
+{
+    uint16_t *newContents;
+    
+    newContents = (uint16_t *)malloc(sizeof(uint16_t) * queueSize);
+    
+    if (newContents == NULL)
+    {
+        return false;
+    }
+    
+    queueP->contents = newContents;
+    queueP->maxSize = queueSize;
+    queueP->cnt = 0;
+    queueP->front = -1;
+    queueP->back = -1;
+}
+
+bool DestroyQueue(queue *queueP)
+{
+    free(queueP->contents);
+    
+    queueP->contents = NULL;
+    queueP->maxSize = 0;
+    queueP->cnt = 0;
+    queueP->front = -1;
+    queueP->back = -1;
+}
+
+bool IsQueueEmpty(queue *queueP)
+{
+    return (bool) (queueP->cnt == 0);
+}
+
+bool IsQueueFull(queue *queueP)
+{
+    return (bool) (queueP->cnt == queueP->maxSize);
+}
+
+bool ClearQueue(queue *queueP)
+{
+    queueP->front = -1;
+    queueP->back = -1;
+    queueP->cnt = 0;
+    
+    return true;
+}
+
+bool PushQueue(queue *queueP, uint16_t element)
+{
+    if (IsQueueFull(queueP))
+    {
+        return false; // We can't push to the queue, its full
+    }
+    else
+    {
+        queueP->back++;
+        queueP->contents[queueP->back % queueP->maxSize] = element;
+        queueP->cnt++;
+        return true;
+    }
+}
+
+uint16_t PullQueue(queue *queueP)
+{
+    if (IsQueueEmpty(queueP))
+    {
+        return NULL;
+    }
+    else
+    {
+        queueP->front++;
+        queueP->cnt--;
+        return queueP->contents[queueP->front % queueP->maxSize];
+    }
+}
---
+++ mcc_generated_files/queue.h
@@ -0,0 +1,68 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef QUEUE_H
+#define	QUEUE_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include "mcc.h"
+
+typedef struct {
+    uint16_t *contents;
+    int front;
+    int back;
+    int maxSize;
+    int cnt;
+} queue;
+
+bool InitQueue(queue *queueP, uint8_t queueSize);
+bool DestroyQueue(queue *queueP);
+bool IsQueueEmpty(queue *queueP);
+bool IsQueueFull(queue *queueP);
+bool ClearQueue(queue *queueP);
+bool PushQueue(queue *queueP, uint16_t element);
+uint16_t PullQueue(queue *queueP);
+
+
+
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
